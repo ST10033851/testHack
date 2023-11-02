33a0971ab92944c836f6f412acd50cb0fa8f8f33
ST10033851 <ST10033851@vcconnect.edu.za>
2023-11-02 19:51:51 +0200

    login

---
+++ BargainBasket/api/login.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require('path');
+const session = require('express-session');
+const crypto = require('crypto');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const User = mongoose.model("User");
+
+export default async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).end("Method Not Allowed");
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+
+    if (user) {
+      // Successful login
+      // You can set cookies or session variables here if needed
+
+      res.writeHead(302, { Location: "/Pages/HomePage.html" });
+      res.end();
+    } else {
+      // Failed login
+      res.writeHead(302, { Location: "/?error=authFailed" });
+      res.end();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).end("Internal Server Error");
+  }
+};
+
