0955094f2130fe2e2b8e29e81a7eab53237891a1
ST10030291 <ST10030291@vcconnect.edu.za>
2023-11-03 09:36:05 +0200

    c10

--- api/SignUpPage.js
+++ api/SignUpPage.js
@@ -1,3 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require('path');
+const session = require('express-session');
+const crypto = require('crypto');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://bargainbasket:bargainbasket123@bargainbasketcluster.gq7cemq.mongodb.net/BargainBasketDB");
+
+const usersSchema = {
+    email: String,
+    username: String,
+    password: String
+}
+
+const User = mongoose.model("User", usersSchema);
+
+
 export default async (req, res) => {
     if (req.method !== "POST") {
         res.status(405).end("Method Not Allowed");
@@ -10,12 +31,10 @@ export default async (req, res) => {
     });
     newUser.save();
 
+    const { email, username, password } = req.body;
+
     try {
-        let newUser = new User({
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password
-        });
+
         newUser.save();
 
         res.writeHead(302, { Location: "/Pages/HomePage.html" });
